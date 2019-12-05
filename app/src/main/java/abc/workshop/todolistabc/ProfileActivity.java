3904c3c470005946bfa151f67c06de9a75a9eeef
Urim <urim.gashi@abc-techgroup.com>
2019-12-05 17:01:20 +0100

    profile back handled

--- app/src/main/java/abc/workshop/todolistabc/ProfileActivity.java
+++ app/src/main/java/abc/workshop/todolistabc/ProfileActivity.java
@@ -3,14 +3,26 @@ package abc.workshop.todolistabc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+    private TextView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        back = findViewById(R.id.back);
+        interactionListener();
+    }
 
-
+    private void interactionListener(){
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
